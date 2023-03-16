8436548319c85486cb40493b38f95735c56f64f5
Chris Pitts <cp25393@gmail.com>
2023-03-16 10:24:16 -0500

    some progress on the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Nav from '../nav';
+import logoVenmo from '../assets/venmo_logo.png';
 
 export default function Home() {
 	return (
@@ -7,7 +8,24 @@ export default function Home() {
 			<Nav page='home' />
 			<main>
 				<h1>San Marcos Abortion Activists</h1>
-				
+				<section id='donate'>
+					<h2>Donate</h2>
+					<div className='container'>
+						<div>
+							<h3>Venmo</h3>
+							<a href='https://account.venmo.com/u/smabortionactivists'>
+								<img src={logoVenmo} alt='venmo' class='logoImg' />
+								<p>@smabortionactivists</p>
+							</a>
+						</div>
+					</div>
+				</section>
+				<section id='socials'>
+					<h2>Follow us on Social Media</h2>
+				</section>
+				<section id='contact'>
+					<h2>Contact Us</h2>
+				</section>
 			</main>
 		</>
 	);
