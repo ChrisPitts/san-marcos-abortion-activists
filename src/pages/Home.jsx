4f0cfa722f5f2a40ade0fe11839f9390639d9f93
Chris Pitts <cp25393@gmail.com>
2023-03-16 15:22:51 -0500

    some styling changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,30 @@ export default function Home() {
 			<Nav page='home' />
 			<main>
 				<h1>San Marcos Abortion Activists</h1>
+				<section id='kits'>
+					<h2>Need a Health Kit?</h2>
+					<article>
+						<p>
+							San Marcos Abortion Activists has partnered with{' '}
+							<a href='#'>The Bridge Collective</a> to deliver free reproductive
+							health kits to San Martians!
+						</p>
+					</article>
+
+					<article>
+						<h3>Kits Include</h3>
+
+						<div class='list-container'>
+							<ul>
+								<li>Plan B Pills</li>
+								<li>A Pregnancy Test</li>
+								<li>Condoms</li>
+							</ul>
+						</div>
+					</article>
+
+					<a class='btn'>Request a Kit</a>
+				</section>
 				<section id='donate'>
 					<h2>Donate</h2>
 					<div className='container'>
