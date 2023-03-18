9a1537a2d894cb612c6e984e94131ada717477a9
Chris Pitts <cp25393@gmail.com>
2023-03-18 11:43:42 -0500

    added media query for repro kit section

--- src/components/Twitter.jsx
+++ src/components/Twitter.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 export default function () {
-  return (
+	return (
 		<a href='https://twitter.com/smabortionact' class='social-component'>
 			<h3>Twitter</h3>
 			<img
