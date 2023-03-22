3e3261429960a3720a1533ef6962199c56b58901
Chris Pitts <cp25393@gmail.com>
2023-03-22 11:07:48 -0500

    added burger menu to navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
 
-export default function Nav(props) {
+export default function Nav(props)
+{
+	const navRef = useRef();
+	function burgerClicked()
+	{
+		console.log('burger clicked');
+		navRef.current.classList.toggle('visible');
+	}
+
 	console.dir(props);
+
+	// TODO burger button should be accessible to screen readers
 	return (
 		<header>
 			<Link to='/'>
 				<img alt='smaa logo' src={logo} />
 			</Link>
-			<nav>
+			<button onClick={burgerClicked} className='burger'>
+				<div className='burger__bar'></div>
+				<div className='burger__bar middle'></div>
+				<div className='burger__bar'></div>
+			</button>
+			<nav ref={navRef}>
 				<Link to='/about' className={props.page == 'about' ? 'selected' : ''}>
 					About
 				</Link>
 				<Link to='/kits' className={props.page == 'kits' ? 'selected' : ''}>
 					Health Kits
 				</Link>
-				<Link to='/resources' className={props.page == 'resources' ? 'selected' : ''}>
+				<Link
+					to='/resources'
+					className={props.page == 'resources' ? 'selected' : ''}
+				>
 					Resources
 				</Link>
 			</nav>
