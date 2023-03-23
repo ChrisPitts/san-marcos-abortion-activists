f135ffbb9fd51750c486d7b1d19122bfb6543c1b
Chris Pitts <cp25393@gmail.com>
2023-03-23 14:13:20 -0500

    changed base route

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-
+// ! Browser router base should be removed once site is put on SMAA domain
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<BrowserRouter>
+	<BrowserRouter basename='/san-marcos-abortion-activists'>
 		<React.StrictMode>
 			<App />
 		</React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base:'/san-marcos-abortion-activists/',
+  base:'/san-marcos-abortion-activists/',
   plugins: [react()],
 })
