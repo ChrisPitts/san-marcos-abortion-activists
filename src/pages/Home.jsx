d341117dd24f3d0a3ec6ce324cd4f5ec9ca30a45
Chris Pitts <cp25393@gmail.com>
2023-04-02 12:41:06 -0500

    added dropdown menu to form

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -58,6 +58,7 @@ export default function Home() {
 				</section>
 				<section id='contact'>
 					<h2>Contact Us</h2>
+					{/* TODO make form send an email */}
 					<form>
 						<label htmlFor='name' className='required'>
 							Name
@@ -65,6 +66,15 @@ export default function Home() {
 						<input type='text' name='name' id='name' required />
 						<label htmlFor='email'>Email</label>
 						<input type='email' name='email' id='email' />
+						<label htmlFor='topic'>Topic / Commttee</label>
+						<select name='topic' id='topic'>
+							<option>General</option>
+							<option>Harassment & Grievance</option>
+							<option>Communications Committee</option>
+							<option>Finance Committee</option>
+							<option>Organizing Committee</option>
+							<option>Policy Committee</option>
+						</select>
 						<label htmlFor='subject'>Subject</label>
 						<input type='text' name='subject' id='subject' />
 						<label htmlFor='message' className='required'>
