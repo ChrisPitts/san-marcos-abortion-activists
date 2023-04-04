1427a16ab8c0cfc61ffc6b54ce2b9cfe1e2c53d9
Chris <cp2533@gmail.com>
2023-04-04 10:48:22 -0500

    added helmets to FormSubmitted and PageError

--- src/pages/FormSubmitted.jsx
+++ src/pages/FormSubmitted.jsx
@@ -1,6 +1,7 @@
 // React
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 
 // Components
 import Nav from "../components/Nav"
@@ -8,6 +9,10 @@ import Nav from "../components/Nav"
 export default function FormSubmitted(){
     return (
         <>
+            <Helmet>
+                <title>Form Submitted</title>
+                <meta name='description' content='Thank You! Your form has been submitted successfully. We will reach out to you as soon as we can!'/>
+            </Helmet>
             <header>
                 <Nav />
             </header>
--- src/pages/PageError.jsx
+++ src/pages/PageError.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import Nav from '../components/Nav';
 
 export default function PageError() {
 	return (
 		<>
+			<Helmet>
+				<title>Page Not Found</title>
+				<meta name='description' content='We could not find the page you were looking for. We apologize for the inconvenience' />
+			</Helmet>
 			<Nav />
 			<main>
                 <h1>Page Not Found</h1>
